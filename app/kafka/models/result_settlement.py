3df8cf45d27d5040239ec0d410f9110b9640a21b
Borislav Kirilov <borislav.kirilov@delasport.com>
2022-06-17 09:34:02 +0300

    [SC-2435] update message structure

--- app/kafka/models/result_settlement.py
+++ app/kafka/models/result_settlement.py
@@ -1,35 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional, List
 
-
-class Odd(BaseModel):
-    id: str
-    selectionStatus: str
-
-    """
-    Converts object literal(dict) to a Odd instance.
-    Args:
-        obj (dict): Object literal(dict)
-    """
-    @staticmethod
-    def from_dict(obj: dict):
-        if obj is None:
-            return None
-
-        return Odd(
-            id=obj.get('id', None),
-            selection_status=obj.get('selectionStatus', None),
-        )
-
 class ResultSettlementMessage(BaseModel):
     id: str
+    selection_status: str
+    odd_id: str
+    market_id: str
     event_id: str
     market_type_id: Optional[str]
     sport_id: Optional[str]
     league_id: Optional[str]
     foreign_key: Optional[str]
     timestamp: Optional[int]
-    odds: List[Odd] = []
 
     """
     Converts object literal(dict) to a ResultSettlementMessage instance.
@@ -42,15 +24,15 @@ class ResultSettlementMessage(BaseModel):
     def from_dict(obj, ctx):
         result_settlement = ResultSettlementMessage(
             id=obj.get('id'),
+            selection_status=obj.get('selectionStatus'),
+            odd_id=obj.get('oddId'),
+            market_id=obj.get('marketId', None),
             event_id=obj.get('eventId', None),
             market_type_id=obj.get('marketTypeId', None),
             sport_id=obj.get('sportId', None),
             league_id=obj.get('leagueId', None),
             foreign_key=obj.get('foreignKey', None),
-            timestamp=obj.get('timestamp', None),
+            timestamp=obj.get('timestamp', None)
         )
-
-        for odd in obj.get('odds', []):
-            result_settlement.odds.append(Odd.from_dict(odd))
 
         return result_settlement
